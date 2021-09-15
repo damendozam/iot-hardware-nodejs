149073bbc00b49caf55377369e7617de5cbb5e88
caasalazarsa <caasalazarsa@unal.edu.co>
2021-09-14 19:36:51 -0500

    envía por mqtt con pyshell

--- index.js
+++ index.js
@@ -2,42 +2,51 @@
 
 var fs = require('fs');
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://192.168.1.105:1883')
+var client  = mqtt.connect('mqtt://localhost')
 var json
 let {PythonShell} = require('python-shell')
 let pyshell = new PythonShell('src/index.py');
 
-pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    json = JSON.parse(message);
-    //console.log(message);
-});
 
 client.on("connect",function(){
-  console.log("Connected");
+      console.log("Connected");
 })
 
-
 client.subscribe('/UMA/hum', function (err) {
-    if (!err) {
-      client.publish('/UMA/hum', json.hum.toString());
-    }
+        if (!err) {
+            console.log("Subscribed");
+        }
 })
 
 
 client.subscribe('/UMA/temp', function (err) {
-    if (!err) {
-      client.publish('/UMA/temp', json.temp.toString());
-    }
+        if (!err) {
+             console.log("Subscribed");
+        }
 })
 
 
+pyshell.on('message', function (message) {
+    // received a message sent from the Python script (a simple "print" statement)
+    json = JSON.parse(message);
+    //console.log(json.hum.toString());
+    //console.log(json.temp.toString());
+
+
+    if( json != undefined){
+        client.publish('/UMA/hum', json.hum.toString());
+        client.publish('/UMA/temp', json.temp.toString());
+    }
+
+
+   
+
+});
 
 client.on('message', function (topic, message) {
-   // message is Buffer
+    // message is Buffer
    console.log(topic.toString());
    console.log(message.toString());
-   client.end();
-})    
-
+   
+}) 
 //module.exports = client;
