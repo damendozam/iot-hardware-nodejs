668a37005b219e8af75a8f1e774f2ae1a141d38c
damendozamUNINPAHU <dmendozamo@uninpahu.edu.co>
2021-09-16 08:00:20 -0500

    Adicion de librerias

--- src/index.py
+++ src/index.py
@@ -1,5 +1,10 @@
+import tsl2561
+import veml6070
+import bme280
+
 import json
 
+"""""
 # a Python object (dict):
 x = {
     "temp": 20,
@@ -12,3 +17,26 @@ y = json.dumps(x)
 # the result is a JSON string:
 while True:
     print(y)
+"""
+
+if __name__ == '__main__':
+    while True:
+        veml = veml6070.Veml6070()
+        uv_raw = veml.get_uva_light_intensity_raw()
+        uv = veml.get_uva_light_intensity()
+        temperature,pressure,humidity = bme280.readBME280All()
+        (ch0,ch1)= tsl2561.readValues()
+
+        x={
+            "uv":uv,
+            "temperature":temperature,
+            "pressure":pressure,
+            "humidity":humidity,
+            "infrared":ch1,
+            "visible":(ch0-ch1)
+        }
+
+        y = json.dumps(x)
+        print(y)
+
+    
